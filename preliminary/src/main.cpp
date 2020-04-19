2923a23d57a599c3894510f6c50afa74ad5495f1
xuefeng <xuefeng1996@163.com>
2020-04-19 22:03:19 +0800

    optimize

--- preliminary/src/main.cpp
+++ preliminary/src/main.cpp
@@ -48,6 +48,7 @@ public:
     }
 
     G_ = new int[280000*50];
+    inv_G_ = new int[280000*255];
     in_degrees_  = new int[ids_num_]; memset(in_degrees_,  0, ids_num_*sizeof(int));
     out_degrees_ = new int[ids_num_]; memset(out_degrees_, 0, ids_num_*sizeof(int));
 
@@ -55,44 +56,70 @@ public:
     for (int i = 0; i < inputs_size; i+=2) {
       from = m[inputs[i]]; to = m[inputs[i+1]];
       G_[from*50+out_degrees_[from]] = to;
+      inv_G_[to*255+in_degrees_[to]] = from;
       in_degrees_[to]++;
       out_degrees_[from]++;
     }
     delete[](inputs);
-    TopoSort(in_degrees_, true);
+
+    int* tmp1  = new int[ids_num_]; memcpy(tmp1,  in_degrees_,  ids_num_*sizeof(int));
+    int* tmp2 = new int[ids_num_]; memcpy(tmp2, out_degrees_, ids_num_*sizeof(int));
+
+    std::queue<int> q;
+    int u, v;
+
+    for (int i = 0; i < ids_num_; i++)
+      if (0 == tmp1[i]) q.push(i);
+    while (!q.empty()) {
+      u = q.front();
+      q.pop();
+      for (int i = 0; i < out_degrees_[u]; i++) {
+        v = G_[u*50+i];
+        if (0 == --tmp1[v]) q.push(v);
+      }
+    }
+    for (int i = 0; i < ids_num_; i++) {
+      if (tmp1[i] == 0) out_degrees_[i] = 0;
+    }
+
+    for (int i = 0; i < ids_num_; i++)
+      if (0 == tmp2[i]) q.push(i);
+    while (!q.empty()) {
+      u = q.front();
+      q.pop();
+      for (int i = 0; i < in_degrees_[u]; i++) {
+        v = inv_G_[u*255+i];
+        if (0 == --tmp2[v]) q.push(v);
+      }
+    }
+    for (int i = 0; i < ids_num_; i++) {
+      if (tmp2[i] == 0) out_degrees_[i] = 0;
+      std::sort(G_+i*50, G_+i*50+out_degrees_[i]);
+      std::sort(inv_G_+i*255, inv_G_+i*255+in_degrees_[i]);
+    }
+
+    delete[](tmp1); delete[](tmp2);
   }
 
   ~DirectedGraph() {
     delete[](ret3_); delete[](ret4_); delete[](ret5_); delete[](ret6_); delete[](ret7_);
     delete[](in_degrees_); delete[](out_degrees_);
     delete[](G_);
+    delete[](inv_G_);
     delete[](status_map_);
-    delete[](reachable_);
   }
 
   void FindAllCycles() {
     int32x4_t p;
 
-    status_map_ = new bool[ids_num_]; memset(status_map_, false, ids_num_*sizeof(bool));
-    reachable_  = new bool[ids_num_]; memset(reachable_,  false, ids_num_*sizeof(bool));
+    status_map_ = new bool[ids_num_*3]; memset(status_map_, false, ids_num_*3*sizeof(bool));
 
     int idx1, idx2, idx3, idx4, idx5, idx6, idx7;
 
     memory_ = std::vector<std::unordered_map<int, std::vector<int>>>(ids_num_);
-    for (idx1 = 0; idx1 < ids_num_; idx1++) {
-      for (int i = 0; i < out_degrees_[idx1]; i++) {
-        idx2 = G_[idx1*50+i];
-        if (idx1 > idx2) memory_[idx2][idx1].emplace_back(-1);
-        for (int j = 0; j < out_degrees_[idx2]; j++) {
-          idx3 = G_[idx2*50+j];
-          if (idx1 > idx3 && idx2 > idx3)
-            memory_[idx3][idx1].emplace_back(idx2);
-        }
-      }
-    }
-
     int local_idx1;
-    std::vector<int> local_idxs1;
+    std::vector<int> local_idxes1;
+    std::vector<int> effective_idxes;
     int bias;
     int path[8];
     for (idx1 = 0; idx1 < ids_num_; idx1++) {
@@ -100,11 +127,28 @@ public:
       if (G_[idx1*50+out_degrees_[idx1]-1] < idx1) continue;
       path[0] = idx1;
 
-      for (auto& tmp : memory_[idx1]) {
-        local_idx1 = tmp.first;
-        if (local_idx1 > idx1) {
-          reachable_[local_idx1] = true;
-          local_idxs1.emplace_back(local_idx1);
+      status_map_[idx1*3+1] = true; effective_idxes.emplace_back(idx1);
+      for (int i = 0; i < in_degrees_[idx1]; i++) {
+        idx2 = inv_G_[idx1*255+i];
+        if (idx2 < idx1) continue;
+        memory_[idx1][idx2].emplace_back(-1);
+        status_map_[idx2*3+1] = true; effective_idxes.emplace_back(idx2);
+        status_map_[idx2*3+2] = true; local_idxes1.emplace_back(idx2);
+
+        for (int j = 0; j < in_degrees_[idx2]; j++) {
+          idx3 = inv_G_[idx2*255+j];
+          if (idx3 < idx1) continue;
+          if (idx3 > idx1) {
+            memory_[idx1][idx3].emplace_back(idx2);
+            status_map_[idx3*3+2] = true; local_idxes1.emplace_back(idx3);
+          }
+          status_map_[idx3*3+1] = true; effective_idxes.emplace_back(idx3);
+
+          for (int k = 0; k < in_degrees_[idx3]; k++) {
+            idx4 = inv_G_[idx3*255+k];
+            if (idx4 < idx1) continue;
+            status_map_[idx4*3+1] = true; effective_idxes.emplace_back(idx4);
+          }
         }
       }
 
@@ -113,13 +157,13 @@ public:
         if (idx2 < idx1 || out_degrees_[idx2] == 0) continue;
         if (G_[idx2*50+out_degrees_[idx2]-1] < idx1) continue;
         path[1] = idx2;
-        status_map_[idx2] = true;
+        status_map_[idx2*3] = true;
 
         for (int j = 0; j < out_degrees_[idx2]; j++) {
           idx3 = G_[idx2*50+j];
           if (idx3 <= idx1) continue;
           path[2] = idx3;
-          status_map_[idx3] = true;
+          status_map_[idx3*3] = true;
 
           for (int k = 0; k < out_degrees_[idx3]; k++) {
             idx4 = G_[idx3*50+k];
@@ -130,26 +174,26 @@ public:
               ret_num_[0]++;
               continue;
             }
-            if (status_map_[idx4] == false) {
+            if (status_map_[idx4*3] == false) {
               path[3] = idx4;
-              status_map_[idx4] = true;
+              status_map_[idx4*3] = true;
 
               for (int l = 0; l < out_degrees_[idx4]; l++) {
                 idx5 = G_[idx4*50+l];
-                if (idx5 < idx1) continue;
+                if (status_map_[idx5*3+1] == false) continue;
                 if (idx5 == idx1) {
                   p = vld1q_s32(path);
                   vst1q_s32(ret_[1]+ret_num_[1]*4, p);
                   ret_num_[1]++;
                   continue;
                 }
-                if (status_map_[idx5] == false) {
+                if (status_map_[idx5*3] == false) {
                   path[4] = idx5;
-                  status_map_[idx5] = true;
+                  status_map_[idx5*3] = true;
 
                   for (int m = 0; m < out_degrees_[idx5]; m++) {
                     idx6 = G_[idx5*50+m];
-                    if (idx6 < idx1) continue;
+                    if (status_map_[idx6*3+1] == false) continue;
                     if (idx6 == idx1) {
                       p = vld1q_s32(path);
                       vst1q_s32(ret_[2]+ret_num_[2]*8, p);
@@ -158,10 +202,10 @@ public:
                       ret_num_[2]++;
                       continue;
                     }
-                    if (reachable_[idx6] == true && status_map_[idx6] == false) {
+                    if (status_map_[idx6*3+2] == true && status_map_[idx6*3] == false) {
                       path[5] = idx6;
                       for (int& idx7 : memory_[idx1][idx6]) {
-                        if (idx7 > 0 && status_map_[idx7] == false) {
+                        if (idx7 > 0 && status_map_[idx7*3] == false) {
                           path[6] = idx7;
                           p = vld1q_s32(path);
                           vst1q_s32(ret_[4]+ret_num_[4]*8, p);
@@ -180,20 +224,24 @@ public:
                       }
                     }
                   }
-                  status_map_[idx5] = false;
+                  status_map_[idx5*3] = false;
                 }
               }
-              status_map_[idx4] = false;
+              status_map_[idx4*3] = false;
             }
           }
-          status_map_[idx3] = false;
+          status_map_[idx3*3] = false;
         }
-        status_map_[idx2] = false;
+        status_map_[idx2*3] = false;
       }
 
-      for (int& local_idx1 : local_idxs1)
-        reachable_[local_idx1] = false;
-      local_idxs1.clear();
+      for (int& local_idx1 : local_idxes1)
+        status_map_[local_idx1*3+2] = false;
+      local_idxes1.clear();
+
+      for (int& idx : effective_idxes)
+        status_map_[idx*3+1] = false;
+      effective_idxes.clear();
     }
   }
 
@@ -227,9 +275,9 @@ private:
   int ids_num_;
 
   int* G_;
+  int* inv_G_;
   std::vector<std::unordered_map<int, std::vector<int>>> memory_;
   bool* status_map_;
-  bool* reachable_;
   int* in_degrees_;
   int* out_degrees_;
   int* ret3_ = new int[4*500000]; 
@@ -240,27 +288,6 @@ private:
   int* ret_[5] = {ret3_, ret4_, ret5_, ret6_, ret7_};
   int ret_num_[5] = {0, 0, 0, 0, 0}; 
   int ret_step_[5] = {4, 4, 8, 8, 8}; 
-
-  void TopoSort(int* degrees, bool do_sorting) {
-    std::queue<int> q;
-    for (int i = 0; i < ids_num_; i++)
-      if (0 == degrees[i]) q.push(i);
-
-    int u, v;
-    while (!q.empty()) {
-      u = q.front();
-      q.pop();
-      for (int i = 0; i < out_degrees_[u]; i++) {
-        v = G_[u*50+i];
-        if (0 == --degrees[v]) q.push(v);
-      }
-    }
-
-    for (int i = 0; i < ids_num_; i++) {
-      if (degrees[i] == 0) out_degrees_[i] = 0;
-      else if (do_sorting) std::sort(G_+i*50, G_+i*50+out_degrees_[i]);
-    }
-  }
 };
 
 

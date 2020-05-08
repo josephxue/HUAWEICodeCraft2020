16929b55a685fec9f7b3105a2d747f9f9a2f2c4f
dingding <dingyh1996@gmail.com>
2020-05-09 01:22:37 +0800

    682.5824

--- semifinal/src/main.cpp
+++ semifinal/src/main.cpp
@@ -22,6 +22,7 @@ public:
   ~Solution() {
     delete[](ids);
     delete[](input_ids_);
+    delete[](ids_comma_); delete[](ids_line_); delete[](sl_);
   }
 
   inline bool check(int x, int y) {
@@ -100,14 +101,25 @@ public:
     std::sort(ids, ids+input_ids_size_);
     ids_num_ = std::unique(ids, ids+input_ids_size_) - ids;
 
+    ids_comma_ = new int8_t[ids_num_*16];
+    ids_line_  = new int8_t[ids_num_*16];
+    sl_= new int[ids_num_];
+
+    unsigned int id, t;
     for (int i = 0; i < ids_num_; i++) {
-      idHash[ids[i]] = i;
+      id = ids[i];
+      idHash[id] = i;
+      t = sprintf((char*)ids_comma_+i*16, "%u,",  id);
+      sprintf((char*)ids_line_+i*16, "%u\n", id);
+      sl_[i] = t;
     }
 
     G = vector<vector<int>>(ids_num_);
     inDegrees = vector<int>(ids_num_, 0);
+
+    int u, v;
     for (int i = 0; i < input_ids_size_; i += 2) {
-      int u = idHash[input_ids_[i]], v = idHash[input_ids_[i+1]];
+      u = idHash[input_ids_[i]]; v = idHash[input_ids_[i+1]];
       G[u].push_back(v);
       ++inDegrees[v];
     }
@@ -117,26 +129,33 @@ public:
   }
 
   //magic code,don't touch
-  bool checkAns(vector<unsigned int> tmp, int depth) {
-      for (int i = 0; i < depth; i++) {
-          int l = tmp[(i + depth - 1) % depth], m = tmp[i], r = tmp[(i + 1) % depth];
-          if (!check(migic[(unsigned long long) l << 32 | m], migic[(unsigned long long) m << 32 | r])) return false;
-      }
-      return true;
+  bool checkAns(int (&path)[7], int depth) {
+    for (int i = 0; i < depth; i++) {
+      int l = ids[path[(i+depth-1)%depth]], m = ids[path[i]], r = ids[path[(i+1)%depth]];
+      if (!check(migic[(unsigned long long)ids[path[(i+depth-1)%depth]] << 32 | ids[path[i]]], 
+                 migic[(unsigned long long) ids[path[i]] << 32 | ids[path[(i+1)%depth]]])) return false;
+    }
+    return true;
   }
 
   void dfs(int head, int cur, int depth, int (&path)[7]) {
     vis[cur] = true;
     path[depth-1] = cur;
     for (int &v:G[cur]) {
-      if (v == head && depth >= 3 && depth <= 7) {
-        vector<unsigned int> tmp;
-        for (int i = 0; i < depth; i++) {
-          tmp.push_back(ids[path[i]]);
+      if (v == head && depth >= 3 && depth <= 7 && checkAns(path, depth)) {
+        for (int i = 0; i < depth-1; i++) {
+          tmpret = vld1q_s8(ids_comma_+path[i]*16); 
+          vst1q_s8(tmps[depth-3], tmpret); 
+          s = sl_[path[i]]; 
+          ret_num_[depth-3] += s; 
+          tmps[depth-3] += s;
         }
-        if (checkAns(tmp, depth)) {
-          ret_[depth-3].emplace_back(tmp);
-        }
+        tmpret = vld1q_s8(ids_line_+path[depth-1]*16); 
+        vst1q_s8(tmps[depth-3], tmpret); 
+        s = sl_[path[depth-1]]; 
+        ret_num_[depth-3] += s; 
+        tmps[depth-3] += s;
+        path_num_++;
       }
       if (depth < 7 && !vis[v] && v > head) {
         dfs(head, v, depth+1, path);
@@ -146,7 +165,6 @@ public:
   }
 
   void solve() {
-    ret_.resize(5);
     vis = vector<bool>(ids_num_, false);
     int path[7];
     for (int i = 0; i < ids_num_; i++) {
@@ -156,18 +174,21 @@ public:
     }
   }
 
-  void save(string &outputFile) {
-    std::ofstream out(outputFile);
-    out << ret_[0].size()+ret_[1].size()+ret_[2].size()+ret_[3].size()+ret_[4].size() << endl;
-    for (int i = 0; i < ret_.size(); i++) {
-      for (auto& path : ret_[i]) {
-        out << path[0];
-        for (int j = 1; j < path.size(); j++) {
-          out << "," << path[j];
-        }
-        out << std::endl;
-      }
+  void save(string &output_filename) {
+    FILE *fp = fopen(output_filename.c_str(), "wb");
+    if (fp == NULL) {
+      printf("file open error\n");
+      exit(1);
     }
+    
+    char buf[15];
+    sprintf(buf, "%d\n", path_num_);
+    fwrite(buf, strlen(buf), sizeof(char), fp);
+
+    for (int i = 0; i < 5; i++)
+      fwrite((char*)ret_[i], ret_num_[i], sizeof(char), fp);
+
+    fclose(fp);
   }
 
 private:
@@ -180,13 +201,27 @@ private:
   vector<int> inDegrees;
   vector<bool> vis;
   int ids_num_;
+  int8_t* ids_comma_;
+  int8_t* ids_line_;
+  int* sl_;
 
-  std::vector<std::vector<std::vector<unsigned int>>> ret_;
-  // std::vector<std::string> ret3_;
-  // std::vector<std::string> ret4_;
-  // std::vector<std::string> ret5_;
-  // std::vector<std::string> ret6_;
-  // std::vector<std::string> ret7_;
+  int8_t* ret3_ = new int8_t[33*20000000]; 
+  int8_t* ret4_ = new int8_t[44*20000000]; 
+  int8_t* ret5_ = new int8_t[55*20000000];
+  int8_t* ret6_ = new int8_t[66*20000000];
+  int8_t* ret7_ = new int8_t[77*20000000];
+  int8_t* ret_[5] = {ret3_, ret4_, ret5_, ret6_, ret7_};
+  int ret_num_[5] = {0, 0, 0, 0, 0}; 
+  int path_num_ = 0;
+
+  int8x16_t tmpret;
+  int8_t* s3 = ret3_;
+  int8_t* s4 = ret4_;
+  int8_t* s5 = ret5_;
+  int8_t* s6 = ret6_;
+  int8_t* s7 = ret7_;
+  int8_t* tmps[5] = {s3, s4, s5, s6, s7};
+  int s;
 };
 
 
